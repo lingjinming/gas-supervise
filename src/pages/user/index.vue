264804cdca42f456df237110c56cdb0f8d79915e
seefly <gitdoit@gmail.com>
2023-06-26 20:11:07 +0800

    feature 个人信息

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,10 +1,67 @@
 <template>
-	<router-tab actPath='pages/user/index'/>
+  <router-tab actPath='pages/user/index' />
+  <view class="container">
+    
+    <van-cell-group>
+      <view class="av">
+        <van-icon name="https://b.yzcdn.cn/vant/icon-demo-1126.png" />
+      </view>
+
+      <van-field :value="form.name" label="姓名" fixed autosize disabled />
+
+      <van-field :value="form.department" label="单位" fixed autosize disabled />
+      
+      <van-field :value="form.gender" label="性别" fixed autosize disabled />
+
+      <van-field :value="form.birthday" label="生日" fixed autosize disabled />
+
+      <van-field :value="form.email" label="邮箱" fixed autosize disabled />
+
+      <van-field :value="form.phone" label="手机" fixed autosize disabled />
+
+      <van-field :value="form.wechat" label="微信" fixed autosize disabled />
+
+      <van-field :value="form.contactAddress" label="联系地址" fixed autosize disabled />
+    </van-cell-group>
 
 
+  </view>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const form = ref({
+  name: '',
+  department: '',
+  gender: '',
+  birthday: '--',
+  email: '',
+  phone: '',
+  contactAddress: '',
+  wechat: '暂无',
+  landline: '暂无'
+});
+
+const userInfo = uni.getStorageSync('USER_INFO');
+  
+  if(userInfo) {
+    form.value.name = userInfo.name || '--';
+    form.value.department = userInfo.organizationVO?.fullName || '--';
+    form.value.gender = userInfo.gender == 'xb002' ? '男' : '女';
+    form.value.phone = userInfo.phoneNumber;
+    form.value.contactAddress = userInfo.contactAddress || '--'
+  }
 
 </script>
 
+<style lang="scss" scoped>
+  .container {
+    margin-top: 30px;
+
+    .av {
+      font-size: 50px;
+      text-align: center;
+    }
+  }
+</style>
