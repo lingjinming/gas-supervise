448384c62918ee74df57b7748a650a82b5950aef
seefly <gitdoit@gmail.com>
2023-06-26 21:03:36 +0800

    feature 隐患分页

--- src/pages/hidden/index.vue
+++ src/pages/hidden/index.vue
@@ -1,14 +1,26 @@
 <template>
     <view class="top">
-        <text>共计16条隐患</text>
+        <text>共计{{ state.total }}条隐患</text>
         <text>筛选</text>
     </view>
-    <hidden></hidden>
+    <hidden v-for="item in state.list" :key="item.uid" :info="item"></hidden>
 </template>
 <script setup lang="ts">
-const notices = [
+import { hidangerPage,type HidangerPgaeVO } from '@/api/hidden';
+import { reactive } from 'vue';
 
-]
+let isOrg: boolean = uni.getStorageSync("USER_INFO")["orgType"] != 1;
+const state = reactive({
+  total: 0,
+  list: [] as HidangerPgaeVO[]
+})
+
+const featchPage = async () => {
+  const {total,data} = await hidangerPage({isOrg});
+  state.total = total;
+  state.list = data;
+}
+featchPage()
 </script>
 <style lang="scss" scoped>
 .top{
