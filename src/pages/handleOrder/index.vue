e99cc1ce302a9de11a03908b356ffb918dac923a
lingjinming <1241790664@qq.com>
2023-08-31 16:21:51 +0800

    完善

--- src/pages/handleOrder/index.vue
+++ src/pages/handleOrder/index.vue
@@ -1,9 +1,16 @@
 <template>
   <!-- 操作步骤 -->
   <view class="step-box">
-    <van-icon name="arrow" custom-style="position:absolute;left:50%;transform:translateX(-50%)" />
-    <view  @click="changeStep(0)"  :class="[data.currentStep===0?'act':'']">选择需要整改的隐患</view>
-    <view  @click="changeStep(1)"  :class="[data.currentStep===1?'act':'']">填写整改单信息</view>
+    <van-icon
+      name="arrow"
+      custom-style="position:absolute;left:50%;transform:translateX(-50%)"
+    />
+    <view @click="changeStep(0)" :class="[data.currentStep === 0 ? 'act' : '']"
+      >选择需要整改的隐患</view
+    >
+    <view @click="changeStep(1)" :class="[data.currentStep === 1 ? 'act' : '']"
+      >填写整改单信息</view
+    >
   </view>
   <!-- 选择隐患/填报整改单 -->
   <view class="handle-order-container">
@@ -16,234 +23,260 @@
       :current="data.currentStep"
       @change="changeStep($event.detail.current)"
     >
-    <!-- 隐患列表 -->
-    <swiper-item >
-      <scroll-view
-        style="height: 85%"
-        scroll-y="true"
-        class="scroll-Y"
-      >
-        <template v-if="total"  v-for="item in list" :key="item.uid">
-          <view  @click.capture.stop="chooseUid(item)">
-            <hidden :info="item">
-              <van-icon   :color="(item.planCode === data.planCode) || data.planCode ==='' ? '#006CFF' : 'gray'" size="36rpx" custom-style="margin-right:20rpx;top:4rpx" :name="data.uids.includes(item.uid) ? 'passed' : 'circle'" />
-            </hidden>
-          </view>
-        </template>
-        <van-empty v-else description="暂无需要处理的隐患"></van-empty>
-      </scroll-view>
-      <van-button
+      <!-- 隐患列表 -->
+      <swiper-item>
+        <scroll-view style="height: 85%" scroll-y="true" class="scroll-Y">
+          <template v-if="total" v-for="item in list" :key="item.uid">
+            <view @click.capture.stop="chooseUid(item)">
+              <hidden :info="item">
+                <van-icon
+                  :color="
+                    item.planCode === data.planCode || data.planCode === ''
+                      ? '#006CFF'
+                      : 'gray'
+                  "
+                  size="36rpx"
+                  custom-style="margin-right:20rpx;top:4rpx"
+                  :name="data.uids.includes(item.uid) ? 'passed' : 'circle'"
+                />
+              </hidden>
+            </view>
+          </template>
+          <van-empty v-else description="暂无需要处理的隐患"></van-empty>
+        </scroll-view>
+        <van-button
           custom-style="width:calc(100% - 80rpx);margin:40rpx 40rpx 0 40rpx;"
           type="primary"
           size="large"
           color="#006CFF"
           @click="changeStep(1)"
           >下一步
-      </van-button>
-    </swiper-item>
-    <!-- 整改单信息填报 -->
-    <swiper-item>
-      <scroll-view
-        style="height: 85%"
-        scroll-y="true"
-        class="scroll-Y"
-      >
-      <view class="tab-detail-wrap">
-        <view class="tab-detail-box">
-          <text class="label">整改单位</text>
-          <text>{{ data.orgName }}</text>
-        </view>
-        <view class="tab-detail-box">
-          <text class="label">联系电话</text>
-          <text>{{ data.phone }}</text>
-        </view>
-        <view class="tab-detail-box">
-          <text class="label">地址</text>
-          <text>{{ data.address }}</text>
-        </view>
-        <van-field required is-link label="整改期限" @click-input="showDatePopup('deadline')" >
-          <input
-            :value="data.order.deadline"
-            slot="input"
-            disabled
-            style="width: 100%"
-            placeholder="请选择"
-          />
-        </van-field>
-        <van-field required is-link label="发送时间" @click-input="showDatePopup('orderDate')" >
-          <input
-            :value="data.order.orderDate"
-            slot="input"
-            disabled
-            style="width: 100%"
-            placeholder="请选择"
-          />
-        </van-field>
+        </van-button>
+      </swiper-item>
+      <!-- 整改单信息填报 -->
+      <swiper-item>
+        <scroll-view style="height: 85%" scroll-y="true" class="scroll-Y">
+          <view class="tab-detail-wrap">
+            <view class="tab-detail-box">
+              <text class="label">整改单位</text>
+              <text>{{ data.orgName }}</text>
+            </view>
+            <view class="tab-detail-box">
+              <text class="label">联系电话</text>
+              <text>{{ data.phone }}</text>
+            </view>
+            <view class="tab-detail-box">
+              <text class="label">地址</text>
+              <text>{{ data.address }}</text>
+            </view>
 
-        <view style="background: #fff">
-          <van-field
-            :value="data.order.expertOpinion"
-            label="专家意见"
-            readonly
-            required
-          />
-          <textarea
-            class="remark"
-            maxlength="200"
-            :value="data.order.expertOpinion"
-            @input="data.order.expertOpinion = $event.detail.value"
-            type="text"
-            placeholder="请输入"
-          />
-        </view>
+            <van-field
+              required
+              is-link
+              label="整改期限"
+              @click-input="showDatePopup('deadline')"
+            >
+              <input
+                :value="data.order.deadline"
+                slot="input"
+                disabled
+                style="width: 100%"
+                placeholder="请选择"
+              />
+            </van-field>
+            <van-field
+              required
+              is-link
+              label="发送时间"
+              @click-input="showDatePopup('orderDate')"
+            >
+              <input
+                :value="data.order.orderDate"
+                slot="input"
+                disabled
+                style="width: 100%"
+                placeholder="请选择"
+              />
+            </van-field>
 
-        <view class="signature-box" @click="navigatoSignature('targetOrgMasterSignatures')" >
-          <van-field
-            title-width="100%"
-            :border="false"
-            required
-            label="企业负责人"
-            readonly
-          />
-          <region-img
-            :disabledPreview="true"
-            v-if="data.targetOrgMasterSignatures"
-            region="test"
-            :id="data.targetOrgMasterSignatures"
-          />
-          <view class="image" v-else>请手写签名</view>
-        </view>
-        <view class="signature-box"  @click="navigatoSignature('expertSignatures1')" >
-          <van-field
-            title-width="100%"
-            :border="false"
-            required
-            label="检查人1(专家)"
-            readonly
-          />
-          <region-img
-            :disabledPreview="true"
-            v-if="data.expertSignatures1"
-            region="test"
-            :id="data.expertSignatures1"
-          />
-          <view class="image" v-else>请手写签名</view>
-        </view>
-        <view class="signature-box" @click="navigatoSignature('expertSignatures2')" >
-          <van-field
-            title-width="100%"
-            :border="false"
-            required
-            label="检查人2(专家)"
-            readonly
-          />
-          <region-img
-            :disabledPreview="true"
-            v-if="data.expertSignatures2"
-            region="test"
-            :id="data.expertSignatures2"
-          />
-          <view class="image" v-else>请手写签名</view>
-        </view>
-      </view>
+            <view style="background: #fff">
+              <van-field
+                :value="data.order.expertOpinion"
+                @change="data.order.expertOpinion = $event.detail"
+                label="专家意见"
+                placeholder="请输入"
+                maxlength="200"
+                type="textarea"
+                :autosize="{
+                  minHeight: 150,
+                }"
+              />
+            </view>
 
-      </scroll-view>
-      <van-button
+            <view
+              class="signature-box"
+              @click="navigatoSignature('targetOrgMasterSignatures')"
+            >
+              <van-field
+                title-width="100%"
+                :border="false"
+                required
+                label="企业负责人"
+                readonly
+              />
+              <region-img
+                :disabledPreview="true"
+                v-if="data.targetOrgMasterSignatures"
+                region="test"
+                :id="data.targetOrgMasterSignatures"
+              />
+              <view class="image" v-else>请手写签名</view>
+            </view>
+            <view
+              class="signature-box"
+              @click="navigatoSignature('expertSignatures1')"
+            >
+              <van-field
+                title-width="100%"
+                :border="false"
+                required
+                label="检查人1(专家)"
+                readonly
+              />
+              <region-img
+                :disabledPreview="true"
+                v-if="data.expertSignatures1"
+                region="test"
+                :id="data.expertSignatures1"
+              />
+              <view class="image" v-else>请手写签名</view>
+            </view>
+            <view
+              class="signature-box"
+              @click="navigatoSignature('expertSignatures2')"
+            >
+              <van-field
+                title-width="100%"
+                :border="false"
+                required
+                label="检查人2(专家)"
+                readonly
+              />
+              <region-img
+                :disabledPreview="true"
+                v-if="data.expertSignatures2"
+                region="test"
+                :id="data.expertSignatures2"
+              />
+              <view class="image" v-else>请手写签名</view>
+            </view>
+          </view>
+        </scroll-view>
+        <van-button
           custom-style="width:calc(100% - 80rpx);margin:40rpx 40rpx 0 40rpx;"
           type="primary"
           size="large"
           color="#006CFF"
           @click="save"
           >保存
-      </van-button>
-    </swiper-item>
+        </van-button>
+      </swiper-item>
     </swiper>
-      <!-- 日历 -->
+    <!-- 日历 -->
     <van-calendar
       :show="data.handleDatePopupIsShow"
       @close="data.handleDatePopupIsShow = false"
       @confirm="chooseDate"
     />
   </view>
-
 </template>
 <script lang="ts" setup>
-import { reactive ,computed} from 'vue';
-import type {HidangerOrgPageQuery,HidangerOrgPageVO,HiDangerSendHandleOrderDTO} from '@/api/generated/data-contracts'
-import {page1,sendHandleOrder} from '@/api/generated/HidangerGov';
-import { detailByPlanCode } from '@/api/generated/HidangerGov'
-import { useTable } from '@/hooks/useTable';
+import { reactive, computed } from "vue";
+import type {
+  HidangerOrgPageQuery,
+  HidangerOrgPageVO,
+  HiDangerSendHandleOrderDTO,
+} from "@/api/generated/data-contracts";
+import { page1, sendHandleOrder } from "@/api/generated/HidangerGov";
+import { detailByPlanCode } from "@/api/generated/HidangerGov";
+import { useTable } from "@/hooks/useTable";
 import { formatDate } from "@/utils";
-import { useLoading } from '@/hooks/useLoading';
+import { useLoading } from "@/hooks/useLoading";
 
-const data  = reactive({
+const data = reactive({
   currentStep: 0,
   handleDatePopupIsShow: false,
-  planCode: '',
+  planCode: "",
   uids: <string[]>[],
-  targetOrgMasterSignatures:'',
-  expertSignatures1: '',
-  expertSignatures2: '',
-  orgName: '',
-  phone: '',
-  address: '',
+  targetOrgMasterSignatures: "",
+  expertSignatures1: "",
+  expertSignatures2: "",
+  orgName: "",
+  phone: "",
+  address: "",
   order: <HiDangerSendHandleOrderDTO>{
     targetOrgMasterSignatures: <string[]>[],
     expertSignatures: <string[]>[],
     dangerIds: <number[]>[],
-    orderDate: '',
-    deadline: '',
-    expertOpinion: '',
-
+    orderDate: "",
+    deadline: "",
+    expertOpinion: "",
   },
   query: <HidangerOrgPageQuery>{
-    planCode: '',
+    planCode: "",
     paging: false,
   },
-})
-const hasSing = computed(() => data.targetOrgMasterSignatures && data.expertSignatures1 && data.expertSignatures2)
-const { total, list, search } = useTable<HidangerOrgPageVO>(data.query, page1, { showToast: true });
+});
+const hasSing = computed(
+  () =>
+    data.targetOrgMasterSignatures &&
+    data.expertSignatures1 &&
+    data.expertSignatures2
+);
+const { total, list, search } = useTable<HidangerOrgPageVO>(data.query, page1, {
+  showToast: true,
+});
 
 onLoad((param) => {
-  data.query.planCode = param!.planCode
-  search()
-})
+  data.query.planCode = param!.planCode;
+  search();
+});
 let dateKey = ref("");
 const chooseDate = (e) => {
   data.handleDatePopupIsShow = false;
   data.order[dateKey.value] = formatDate(e.detail);
 };
 const showDatePopup = (key) => {
+  console.log(key);
   data.handleDatePopupIsShow = true;
   dateKey.value = key;
 };
-const changeStep = (step : number) => {
+const changeStep = (step: number) => {
   // 判断是否选择隐患
-  if(!data.uids.length) {
+  if (!data.uids.length) {
     uni.showToast({
       icon: "none",
       title: "请选择要整改的隐患!",
     });
     return;
-  };
-  const result = detailByPlanCode({planCode: data.planCode});
-  useLoading(result,(result) => {
+  }
+  const result = detailByPlanCode({ planCode: data.planCode });
+  useLoading(result, (result) => {
     data.currentStep = step;
     data.address = result.targetOrgAddr;
     data.orgName = result._targetOrgId;
     data.phone = result.targetOrgPhone;
-  })
-  
-}
+  });
+};
 
 const chooseUid = (item: HidangerOrgPageVO) => {
-  if(!data.planCode || data.planCode === item.planCode) {
+  if (!data.planCode || data.planCode === item.planCode) {
     data.planCode = item.planCode;
-    if(data.uids.includes(item.uid)) {
+    if (data.uids.includes(item.uid)) {
       data.uids.splice(data.uids.indexOf(item.uid), 1);
-      data.planCode = '';
+      data.planCode = "";
     } else {
-      data.uids.push(item.uid)
+      data.uids.push(item.uid);
     }
   } else {
     uni.showToast({
@@ -251,23 +284,21 @@ const chooseUid = (item: HidangerOrgPageVO) => {
       title: "只能选择同一个检查计划下的隐患!",
     });
   }
-  
-  
-}
+};
 const navigatoSignature = (key) => {
   uni.navigateTo({
     url: `/pages/signature/index?key=${key}`,
     events: {
       onSignature: (result) => {
         data[key] = result.data.objectName;
-      }
-    }
+      },
+    },
   });
 };
 
 const save = () => {
   // 判断是否选择隐患
-  if(!data.uids.length) {
+  if (!data.uids.length) {
     uni.showToast({
       icon: "none",
       title: "请选择要整改的隐患!",
@@ -275,7 +306,7 @@ const save = () => {
     return;
   }
   // 判断是否签字
-  if(!hasSing) {
+  if (!hasSing) {
     uni.showToast({
       icon: "none",
       title: "请手写签字!",
@@ -283,20 +314,21 @@ const save = () => {
     return;
   }
   // 组织数据
-  data.order.expertSignatures.push(data.expertSignatures1,data.expertSignatures2);
+  data.order.expertSignatures.push(
+    data.expertSignatures1,
+    data.expertSignatures2
+  );
   data.order.targetOrgMasterSignatures.push(data.targetOrgMasterSignatures);
   // @ts-ignore
-  data.order.dangerIds.push(...data.uids)
+  data.order.dangerIds.push(...data.uids);
 
-  console.log(data.order);
   const result = sendHandleOrder(data.order);
-  useLoading(result,() => {
+  useLoading(result, () => {
     uni.redirectTo({
-      url: '/pages/check/index',
+      url: "/pages/check/index",
     });
-  })
-
-}
+  });
+};
 </script>
 <style lang="scss" scoped>
 .step-box {
