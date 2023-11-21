52d16e601b321578a6a7c5bdfaeb10264135faab
liujianxin <gitdoit@gmail.com>
2023-11-21 16:24:49 +0800

    bug fix

--- src/pages/handleOrder/index.vue
+++ src/pages/handleOrder/index.vue
@@ -1,56 +1,32 @@
 <template>
   <!-- 操作步骤 -->
   <view class="step-box">
-    <van-icon
-      name="arrow"
-      custom-style="position:absolute;left:50%;transform:translateX(-50%)"
-    />
-    <view @click="changeStep(0)" :class="[data.currentStep === 0 ? 'act' : '']"
-      >选择需要整改的隐患</view
-    >
-    <view @click="changeStep(1)" :class="[data.currentStep === 1 ? 'act' : '']"
-      >填写整改单信息</view
-    >
+    <van-icon name="arrow" custom-style="position:absolute;left:50%;transform:translateX(-50%)" />
+    <view @click="changeStep(0)" :class="[data.currentStep === 0 ? 'act' : '']">选择需要整改的隐患</view>
+    <view @click="changeStep(1)" :class="[data.currentStep === 1 ? 'act' : '']">填写整改单信息</view>
   </view>
   <!-- 选择隐患/填报整改单 -->
   <view class="handle-order-container">
-    <swiper
-      class="swiper"
-      :disable-touch="true"
-      :circular="false"
-      :indicator-dots="false"
-      :autoplay="false"
-      :current="data.currentStep"
-      @change="changeStep($event.detail.current)"
-    >
+    <swiper class="swiper" :disable-touch="true" :circular="false" :indicator-dots="false" :autoplay="false"
+      :current="data.currentStep" @change="changeStep($event.detail.current)">
       <!-- 隐患列表 -->
       <swiper-item>
         <scroll-view style="height: 85%" scroll-y="true" class="scroll-Y">
-          <template v-if="total" v-for="item in list" :key="item.uid">
+          <template v-if="list.length" v-for="item in list" :key="item.uid">
             <view @click.capture.stop="chooseUid(item)">
               <hidden :info="item">
-                <van-icon
-                  :color="
-                    item.planCode === data.planCode || data.planCode === ''
-                      ? '#006CFF'
-                      : 'gray'
-                  "
-                  size="36rpx"
-                  custom-style="margin-right:20rpx;top:4rpx"
-                  :name="data.uids.includes(item.uid) ? 'passed' : 'circle'"
-                />
+                <van-icon :color="item.planCode === data.planCode || data.planCode === ''
+                    ? '#006CFF'
+                    : 'gray'
+                  " size="36rpx" custom-style="margin-right:20rpx;top:4rpx"
+                  :name="data.uids.includes(item.uid) ? 'passed' : 'circle'" />
               </hidden>
             </view>
           </template>
           <van-empty v-else description="暂无需要处理的隐患"></van-empty>
         </scroll-view>
-        <van-button
-          custom-style="width:calc(100% - 80rpx);margin:40rpx 40rpx 0 40rpx;"
-          type="primary"
-          size="large"
-          color="#006CFF"
-          @click="changeStep(1)"
-          >下一步
+        <van-button custom-style="width:calc(100% - 80rpx);margin:40rpx 40rpx 0 40rpx;" type="primary" size="large"
+          color="#006CFF" @click="changeStep(1)">下一步
         </van-button>
       </swiper-item>
       <!-- 整改单信息填报 -->
@@ -70,157 +46,57 @@
               <text>{{ data.address }}</text>
             </view>
 
-            <van-field
-              required
-              is-link
-              label="整改期限"
-              @click-input="showDatePopup('deadline')"
-            >
-              <input
-                :value="data.order.deadline"
-                slot="input"
-                disabled
-                style="width: 100%"
-                placeholder="请选择"
-              />
+            <van-field required is-link label="整改期限" @click-input="showDatePopup('deadline')">
+              <input :value="data.order.deadline" slot="input" disabled style="width: 100%" placeholder="请选择" />
             </van-field>
-            <van-field
-              required
-              is-link
-              label="发送时间"
-              @click-input="showDatePopup('orderDate')"
-            >
-              <input
-                :value="data.order.orderDate"
-                slot="input"
-                disabled
-                style="width: 100%"
-                placeholder="请选择"
-              />
+            <van-field required is-link label="发送时间" @click-input="showDatePopup('orderDate')">
+              <input :value="data.order.orderDate" slot="input" disabled style="width: 100%" placeholder="请选择" />
             </van-field>
 
-        <view class="signature-box" @click="navigatoSignature('targetOrgMasterSignatures')" >
-          <van-field
-            title-width="100%"
-            :border="false"
-            required
-            label="企业负责人"
-            readonly
-          />
-          <region-img
-            :disabledPreview="true"
-            v-if="data.targetOrgMasterSignatures"
-            :id="data.targetOrgMasterSignatures"
-          />
-          <view class="image" v-else>请手写签名</view>
-        </view>
-        <view class="signature-box"  @click="navigatoSignature('expertSignatures1')" >
-          <van-field
-            title-width="100%"
-            :border="false"
-            required
-            label="检查人1(专家)"
-            readonly
-          />
-          <region-img
-            :disabledPreview="true"
-            v-if="data.expertSignatures1"
-            :id="data.expertSignatures1"
-          />
-          <view class="image" v-else>请手写签名</view>
-        </view>
-        <view class="signature-box" @click="navigatoSignature('expertSignatures2')" >
-          <van-field
-            title-width="100%"
-            :border="false"
-            required
-            label="检查人2(专家)"
-            readonly
-          />
-          <region-img
-            :disabledPreview="true"
-            v-if="data.expertSignatures2"
-            :id="data.expertSignatures2"
-          />
-          <view class="image" v-else>请手写签名</view>
-        </view>
-      </view>
-
-            <view
-              class="signature-box"
-              @click="navigatoSignature('targetOrgMasterSignatures')"
-            >
-              <van-field
-                title-width="100%"
-                :border="false"
-                required
-                label="企业负责人"
-                readonly
-              />
-              <region-img
-                :disabledPreview="true"
-                v-if="data.targetOrgMasterSignatures"
-                region="test"
-                :id="data.targetOrgMasterSignatures"
-              />
+            <view class="signature-box" @click="navigatoSignature('targetOrgMasterSignatures')">
+              <van-field title-width="100%" :border="false" required label="企业负责人" readonly />
+              <region-img :disabledPreview="true" v-if="data.targetOrgMasterSignatures"
+                :id="data.targetOrgMasterSignatures" />
               <view class="image" v-else>请手写签名</view>
             </view>
-            <view
-              class="signature-box"
-              @click="navigatoSignature('expertSignatures1')"
-            >
-              <van-field
-                title-width="100%"
-                :border="false"
-                required
-                label="检查人1(专家)"
-                readonly
-              />
-              <region-img
-                :disabledPreview="true"
-                v-if="data.expertSignatures1"
-                region="test"
-                :id="data.expertSignatures1"
-              />
+            <view class="signature-box" @click="navigatoSignature('expertSignatures1')">
+              <van-field title-width="100%" :border="false" required label="检查人1(专家)" readonly />
+              <region-img :disabledPreview="true" v-if="data.expertSignatures1" :id="data.expertSignatures1" />
               <view class="image" v-else>请手写签名</view>
             </view>
-            <view
-              class="signature-box"
-              @click="navigatoSignature('expertSignatures2')"
-            >
-              <van-field
-                title-width="100%"
-                :border="false"
-                required
-                label="检查人2(专家)"
-                readonly
-              />
-              <region-img
-                :disabledPreview="true"
-                v-if="data.expertSignatures2"
-                region="test"
-                :id="data.expertSignatures2"
-              />
+            <view class="signature-box" @click="navigatoSignature('expertSignatures2')">
+              <van-field title-width="100%" :border="false" required label="检查人2(专家)" readonly />
+              <region-img :disabledPreview="true" v-if="data.expertSignatures2" :id="data.expertSignatures2" />
               <view class="image" v-else>请手写签名</view>
             </view>
           </view>
+
+          <view class="signature-box" @click="navigatoSignature('targetOrgMasterSignatures')">
+            <van-field title-width="100%" :border="false" required label="企业负责人" readonly />
+            <region-img :disabledPreview="true" v-if="data.targetOrgMasterSignatures" region="test"
+              :id="data.targetOrgMasterSignatures" />
+            <view class="image" v-else>请手写签名</view>
+          </view>
+          <view class="signature-box" @click="navigatoSignature('expertSignatures1')">
+            <van-field title-width="100%" :border="false" required label="检查人1(专家)" readonly />
+            <region-img :disabledPreview="true" v-if="data.expertSignatures1" region="test"
+              :id="data.expertSignatures1" />
+            <view class="image" v-else>请手写签名</view>
+          </view>
+          <view class="signature-box" @click="navigatoSignature('expertSignatures2')">
+            <van-field title-width="100%" :border="false" required label="检查人2(专家)" readonly />
+            <region-img :disabledPreview="true" v-if="data.expertSignatures2" region="test"
+              :id="data.expertSignatures2" />
+            <view class="image" v-else>请手写签名</view>
+          </view>
         </scroll-view>
-        <van-button
-          custom-style="width:calc(100% - 80rpx);margin:40rpx 40rpx 0 40rpx;"
-          type="primary"
-          size="large"
-          color="#006CFF"
-          @click="save"
-          >保存
+        <van-button custom-style="width:calc(100% - 80rpx);margin:40rpx 40rpx 0 40rpx;" type="primary" size="large"
+          color="#006CFF" @click="save">保存
         </van-button>
       </swiper-item>
     </swiper>
     <!-- 日历 -->
-    <van-calendar
-      :show="data.handleDatePopupIsShow"
-      @close="data.handleDatePopupIsShow = false"
-      @confirm="chooseDate"
-    />
+    <van-calendar :show="data.handleDatePopupIsShow" @close="data.handleDatePopupIsShow = false" @confirm="chooseDate" />
   </view>
 </template>
 <script lang="ts" setup>
@@ -373,20 +249,25 @@ const save = () => {
   line-height: 100rpx;
   background: #fff;
   gap: 85rpx;
+
   .act {
     color: $uni-color-primary;
   }
 }
+
 .handle-order-container {
   height: calc(100% - 120rpx);
   overflow: hidden;
+
   .swiper {
     height: 100%;
   }
 }
+
 .signature-box {
   position: relative;
   height: 400rpx;
+
   &::after {
     position: absolute;
     content: "";
@@ -396,17 +277,20 @@ const save = () => {
     height: 2rpx;
     border-bottom: 1rpx solid $uni-border-color;
   }
+
   &:last-child {
     &::after {
       display: none;
     }
   }
+
   ::v-deep .image,
   image {
     margin: 0 30rpx;
     width: calc(100% - 60rpx);
     height: calc(100% - 100rpx);
   }
+
   ::v-deep .image {
     @include flex-center;
     background: #fbf8f3;
@@ -414,9 +298,11 @@ const save = () => {
     filter: blur(1px);
   }
 }
+
 .tab-detail-wrap {
   height: 100%;
   overflow-y: auto;
+
   .label {
     font-size: 24rpx;
     padding-left: 34rpx;
